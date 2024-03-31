a9b78fdad5f4bfe96b9e88953e6e1b1df80cc1ca
wxfeiang <mac@wangpengdeMacBook-Pro-2017.local>
2024-03-31 13:46:04 +0800

    feat: 更新公共模块的服务

--- src/base/base.controller.ts
+++ src/base/base.controller.ts
@@ -9,28 +9,21 @@ import {
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { isPublic } from 'src/auth/constants';
-import { getConfig } from 'src/common/utils/ymlConfig';
-import * as svgCaptcha from 'svg-captcha';
+
+import { BaseService } from './base.service';
 
 @ApiTags('公共模块')
 @Controller('base')
 export class BaseController {
+  constructor(private readonly baseService: BaseService) {}
+
   @ApiOperation({
     summary: '获取图形验证码',
   })
   @isPublic()
   @Post('/captchaImage')
   captchaImage(@Session() session) {
-    const captcha = svgCaptcha.create({
-      size: 4, //验证码长度
-      fontSize: 50,
-      width: 120,
-      height: 30,
-      background: '#f5f5f5', //背景颜色
-    });
-
-    session.code = captcha.text; //session保存验证码
-    return { img: captcha.data, text: captcha.text }; // aptcha.data  返回的是svg图
+    return this.baseService.captchaImage(session);
   }
 
   @ApiOperation({
@@ -43,14 +36,6 @@ export class BaseController {
     @UploadedFile() file: Express.Multer.File,
     @Headers('host') host: string,
   ) {
-    // 如果是 localhost 就加上http://
-    if (!host.includes('://')) {
-      host = `http://${host}/v${getConfig('VERSION').index}/`;
-    }
-    const data = {
-      url: file.path,
-      base: host,
-    };
-    return data;
+    return this.baseService.uploadLocal(file, host);
   }
 }
--- src/base/base.service.ts
+++ src/base/base.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
-
+import { getConfig } from 'src/common/utils/ymlConfig';
+import * as svgCaptcha from 'svg-captcha';
 @Injectable()
-export class BaseService {}
+export class BaseService {
+  // 验证码
+  async captchaImage(session) {
+    const captcha = svgCaptcha.create({
+      size: 4, //验证码长度
+      fontSize: 50,
+      width: 120,
+      height: 30,
+      background: '#f5f5f5', //背景颜色
+    });
+
+    session.code = captcha.text; //session保存验证码
+    return { img: captcha.data, text: captcha.text }; // aptcha.data  返回的是svg图
+  }
+  // 本地上传
+  async uploadLocal(file, host) {
+    // 如果是 localhost 就加上http://
+    if (!host.includes('://')) {
+      host = `http://${host}/v${getConfig('VERSION').index}/`;
+    }
+    const data = {
+      url: file.path,
+      base: host,
+    };
+    return data;
+  }
+}
